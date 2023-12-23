87675f002fedd762eb296567ae0d63c763936d16
Sebastian Kuzminsky <seb@highlab.com>
2023-12-23 09:46:16 -0700

    move rust submodules out of the rust project dir
    Having the submodules in the old place confused cargo and prevented me
    from building the submodules by themselves.

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -63,8 +63,8 @@ async fn main(spawner: Spawner) {
     // Set up the Wifi.
     //
 
-    let fw = include_bytes!("../submodules/embassy/cyw43-firmware/43439A0.bin");
-    let clm = include_bytes!("../submodules/embassy/cyw43-firmware/43439A0_clm.bin");
+    let fw = include_bytes!("../../submodules/embassy/cyw43-firmware/43439A0.bin");
+    let clm = include_bytes!("../../submodules/embassy/cyw43-firmware/43439A0_clm.bin");
 
     // To make flashing faster for development, you may want to flash the firmwares independently
     // at hardcoded addresses, instead of baking them into the program with `include_bytes!`:
